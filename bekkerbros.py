aefddc9e3dc4221c76f42cb0d5bf8eb9b59c9071
Ruan Bekker <ruan@globee.com>
2022-02-12 01:24:11 +0200

    add colors and event loops

--- bekkerbros.py
+++ bekkerbros.py
@@ -11,7 +11,8 @@ test_font = pygame.font.Font('src/font/pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('src/graphics/sky.png').convert()
 ground_surface = pygame.image.load('src/graphics/ground.png').convert()
-text_surface = test_font.render('Welcome to BekkerBros', False, 'Black')
+text_surface = test_font.render('Welcome to BekkerBros', False, (64,64,64))
+score_rectangle = text_surface.get_rect(center = (400, 50))
 
 bro_surface = pygame.image.load('src/graphics/character_pixelated.png').convert_alpha()
 #bro_x_pos = 600
@@ -25,9 +26,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rectangle.collidepoint(event.pos):
+        #         print(event.pos)
+
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(230,50))
+    pygame.draw.rect(screen,'#c0e8ec',score_rectangle)
+    pygame.draw.rect(screen,'#c0e8ec',score_rectangle,10)
+    screen.blit(text_surface,score_rectangle)
     # get position
     #print(player_rectangle.left)
     bro_rectangle.x -= 4
@@ -35,11 +42,13 @@ while True:
     screen.blit(bro_surface,bro_rectangle)
     screen.blit(player_surface,player_rectangle)
 
-    if player_rectangle.colliderect(bro_rectangle):
-        print('collision')
+    mouse_position = pygame.mouse.get_pos()
+    if player_rectangle.collidepoint(mouse_position):
+       print(pygame.mouse.get_pressed()) # prints mouse action
+       print('collision')
 
     #if player_rectangle.collidepoint((x,y)):
 
-
     pygame.display.update()
     clock.tick(60)
+
