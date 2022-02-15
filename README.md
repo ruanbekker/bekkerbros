9c8896ec46d2e1d14cd3a92674fdc4d189d5779a
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-02-15 08:48:54 +0200

    Transforming surfaces

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ Game in PyGame
 Images:
 
 - https://publicdomainvectors.org/en/free-clipart/Pixel-character/40464.html
+- https://opengameart.org/content/enemies-and-characters-pixel-art
+- https://es.pixilart.com/okamike/gallery
+- https://opengameart.org/content/2d-enemy-characters-pack-20x20
+- https://route1rodent.itch.io/16x16-rpg-character-sprite-sheet
+- https://gentoogames.itch.io/aztec-asset-pack
+- https://itch.io/profile/pixeltier
+- https://opengameart.org/content/twelve-16x18-rpg-sprites-plus-base
 
 Tutorial:
 
-- https://youtu.be/AY9MnQ4x3zk?t=6961
+- https://youtu.be/AY9MnQ4x3zk?t=8619
+
+Courses:
+
+- https://www.raywenderlich.com/2888-introduction-to-pixel-art-for-games
--- bekkerbros.py
+++ bekkerbros.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from turtle import width
 import pygame
 from sys import exit
 
@@ -8,7 +9,7 @@ def display_score():
     score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
     score_rectangle = score_surface.get_rect(center = (400, 50))
     screen.blit(score_surface, score_rectangle)
-    #print(current_time)
+    return current_time
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -16,8 +17,9 @@ pygame.display.set_caption('Bekker Bros')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('src/font/pixeltype.ttf', 50)
 
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 # Background surfaces
 sky_surface = pygame.image.load('src/graphics/sky.png').convert()
@@ -27,13 +29,23 @@ ground_surface = pygame.image.load('src/graphics/ground.png').convert()
 # text_surface = test_font.render('Welcome to BekkerBros', False, (64,64,64))
 # score_rectangle = text_surface.get_rect(center = (400, 50))
 
-bro_surface = pygame.image.load('src/graphics/character_pixelated.png').convert_alpha()
-bro_rectangle = bro_surface.get_rect(bottomright=(600,300)) # where do we place the rectangle
+enemy_surface = pygame.image.load('src/graphics/character_pixelated.png').convert_alpha()
+enemy_rectangle = enemy_surface.get_rect(bottomright=(600,300)) # where do we place the rectangle
 
-player_surface = pygame.image.load('src/graphics/enemies/monster_walk_1.png').convert_alpha()
+player_surface = pygame.image.load('src/graphics/player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom=(80,300))
 player_gravty = 0
 
+# Intro screen
+player_stand = pygame.image.load('src/graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rectangle = player_stand.get_rect(center = (400, 200))
+
+game_name = test_font.render('BekkerBros', False, (111, 196, 169))
+game_name_rectangle = game_name.get_rect(center = (400, 80))
+
+game_message = test_font.render('Press Space to Start', False, (111, 196, 169))
+game_message_rectangle = game_message.get_rect(center = (400, 320))
 
 while True:
     for event in pygame.event.get():
@@ -52,34 +64,44 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                bro_rectangle.left = 800
+                enemy_rectangle.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
-        screen.blit(sky_surface,(0,0))
-        screen.blit(ground_surface,(0,300))
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
         # pygame.draw.rect(screen,'#c0e8ec',score_rectangle)
         # pygame.draw.rect(screen,'#c0e8ec',score_rectangle,10)
         # screen.blit(text_surface,score_rectangle)
-        display_score()
+        score = display_score()
 
-        bro_rectangle.x -= 4
-        if bro_rectangle.right < 0: bro_rectangle.left = 800
-        screen.blit(bro_surface,bro_rectangle)
+        enemy_rectangle.x -= 4
+        if enemy_rectangle.right < 0: enemy_rectangle.left = 800
+        screen.blit(enemy_surface, enemy_rectangle)
 
         # Player
         player_gravty += 1
         player_rectangle.y += player_gravty
         if player_rectangle.bottom >= 300: player_rectangle.bottom = 300
-        screen.blit(player_surface,player_rectangle)
+        screen.blit(player_surface, player_rectangle)
 
         # Collisions
-        if bro_rectangle.colliderect(player_rectangle):
+        if enemy_rectangle.colliderect(player_rectangle):
             print('collision detected, game over')
             game_active = False
     
     else:
-        screen.fill('Black')
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rectangle)
+
+        score_message = test_font.render(f'Your Score: {score}', False, (111, 196, 169))
+        score_message_rectangle = score_message.get_rect(center = (400, 330))
+        screen.blit(game_name, game_name_rectangle)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rectangle)
+        else:
+            screen.blit(score_message, score_message_rectangle)
 
     pygame.display.update()
     clock.tick(60)
